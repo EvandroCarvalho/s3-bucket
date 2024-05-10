6fd851473ac6cd3d6f54674bfd5fc90ed8eeabbe
Evandro Junior <evandrojunior@gmail.com>
2024-05-10 00:37:32 -0300

    destroy bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,54 +17,54 @@ resource "random_string" "random" {
 #   force_destroy = true
 # }
 
-resource "aws_s3_bucket_website_configuration" "blog" {
-  bucket = aws_s3_bucket.bucket.id
-  index_document {
-    suffix = "index.html"
-  }
-  error_document {
-    key = "error.html"
-  }
-}
+# resource "aws_s3_bucket_website_configuration" "blog" {
+#   bucket = aws_s3_bucket.bucket.id
+#   index_document {
+#     suffix = "index.html"
+#   }
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "public_access_block" {
+#   bucket = aws_s3_bucket.bucket.id
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-##### will upload all the files present under HTML folder to the S3 bucket #####
-resource "aws_s3_object" "upload_object" {
-  for_each      = fileset("public/", "*")
-  bucket        = aws_s3_bucket.bucket.id
-  key           = each.value
-  source        = "public/${each.value}"
-  etag          = filemd5("public/${each.value}")
-  content_type  = "text/html"
-}
+# ##### will upload all the files present under HTML folder to the S3 bucket #####
+# resource "aws_s3_object" "upload_object" {
+#   for_each      = fileset("public/", "*")
+#   bucket        = aws_s3_bucket.bucket.id
+#   key           = each.value
+#   source        = "public/${each.value}"
+#   etag          = filemd5("public/${each.value}")
+#   content_type  = "text/html"
+# }
 
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
+# resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+#   bucket = aws_s3_bucket.bucket.id
+#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+# }
 
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
+# data "aws_iam_policy_document" "allow_access_from_another_account" {
+#   statement {
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["*"]
+#     }
 
-    actions = [
-      "s3:GetObject"
-    ]
+#     actions = [
+#       "s3:GetObject"
+#     ]
 
-    resources = [
-      aws_s3_bucket.bucket.arn,
-      "${aws_s3_bucket.bucket.arn}/*",
-    ]
-  }
-}
+#     resources = [
+#       aws_s3_bucket.bucket.arn,
+#       "${aws_s3_bucket.bucket.arn}/*",
+#     ]
+#   }
+# }
