6bffe8021c3349c1395f6e16c1f5775bf5660a54
Evandro Junior <evandrojunior@gmail.com>
2024-05-09 23:44:14 -0300

    add terraform plan action

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+# required for AWS
+variable "aws_access_key_id" {
+    description = "Access key to AWS console"
+    default = ""
+}
+variable "aws_access_secret_id" {
+    description = "Secret key to AWS console"
+    default = ""
+}
+variable "region" {
+    description = "Region of AWS VPC"
+    default = ""
+}
+
+variable "account_number" {
+    description = "AWS account number"
+    default = "947810884862"
+}
